1f0c2a2bedf0b8442d6aca6c2de6c5522c3229bd
Marc <celatzur@gmail.com>
2019-01-15 00:05:26 +0100

    Update WashingMachine_v1.02.ino

--- WashingMachine_v1.02.ino
+++ WashingMachine_v1.02.ino
@@ -250,6 +250,25 @@ void httpServer(){
   Serial.println("");
 }
 
+/* Next functions are from the basic ESP8266 HelloServer example*/
+
 void handleRoot() {
   server.send(200, "text/plain", "Hello from Washing Machine!");
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
 }
