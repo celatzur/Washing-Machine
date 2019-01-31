7e8d900263a4d9c31b3ab33b8e23249130cca2fd
Marc <celatzur@gmail.com>
2019-01-31 22:19:09 +0100

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,16 @@
+// Use this file to store all of the private credentials and connection details before sharing the code
+
+#define SECRET_SSID "MySSID"		        // replace MySSID with your WiFi network name. Max 32 Char
+#define SECRET_PASS "MyPassword"	      // replace MyPassword with your WiFi password
+
+#define SECRET_USER_MAIL "yourEmail@gmail.com"
+#define SECRET_USER_MAIL_PASS "yourEmail_password"
+
+#define SECRET_FROM_MAIL "MAIL From:<FromEmail@gmail.com>"
+#define SECRET_TO_MAIL "RCPT To:<yourEmail@gmail.com>"
+#define SECRET_SMTP_SERVER "smtp.mail.gmail.com"            // SMTP server address smtp.gmail.com
+
+#define SECRET_HOST_IP "192.168.*.***"  // The serial port will tell you the IP once it starts up
+                                        // just write it here afterwards and upload
+
+                                            
